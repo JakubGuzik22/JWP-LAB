9ffb862276be175b0b6a5596128ccb521fcba6c1
JakubGuzik22 <148846333+JakubGuzik22@users.noreply.github.com>
2025-03-13 12:13:42 +0100

    Update MainWindow.xaml.cs

--- 1s4/MainWindow.xaml.cs
+++ 1s4/MainWindow.xaml.cs
@@ -83,3 +83,105 @@ namespace _1s4
         }
     }
 }
+
+
+
+
+
+
+Oto kod w języku C# dla aplikacji WPF, który definiuje metodę Prostokat, oblicza pole, obwód i przekątną prostokąta na podstawie wartości wprowadzonych w TextBox i wyświetla wynik w MessageBox.
+
+Kod XAML (MainWindow.xaml)
+
+Dodaj dwa TextBox do wprowadzania szerokości i wysokości, oraz Button, który wywoła test metody.
+
+<Window x:Class="WpfApp.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Prostokąt" Height="200" Width="300">
+    <Grid>
+        <StackPanel Margin="20">
+            <TextBox x:Name="txtSzerokosc" PlaceholderText="Podaj szerokość"/>
+            <TextBox x:Name="txtWysokosc" PlaceholderText="Podaj wysokość"/>
+            <Button Content="Test3" Click="Test3_Click"/>
+        </StackPanel>
+    </Grid>
+</Window>
+
+Kod C# (MainWindow.xaml.cs)
+
+using System;
+using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Metoda Prostokat obliczająca pole, obwód i przekątną
+        void Prostokat(double szerokosc, double wysokosc, out double pole, out double obwod, out double przekatna)
+        {
+            pole = szerokosc * wysokosc;
+            obwod = 2 * (szerokosc + wysokosc);
+            przekatna = Math.Sqrt(szerokosc * szerokosc + wysokosc * wysokosc);
+        }
+
+        private void Test3_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtSzerokosc.Text, out double szerokosc) &&
+                double.TryParse(txtWysokosc.Text, out double wysokosc))
+            {
+                Prostokat(szerokosc, wysokosc, out double pole, out double obwod, out double przekatna);
+
+                string wynik = $"Pole: {pole:F2}\nObwód: {obwod:F2}\nPrzekątna: {przekatna:F2}";
+                MessageBox.Show(wynik, "Wyniki", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź poprawne liczby!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
+
+using System;
+using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Metoda Prostokat obliczająca pole, obwód i przekątną
+        void Prostokat(double szerokosc, double wysokosc, out double pole, out double obwod, out double przekatna)
+        {
+            pole = szerokosc * wysokosc;
+            obwod = 2 * (szerokosc + wysokosc);
+            przekatna = Math.Sqrt(szerokosc * szerokosc + wysokosc * wysokosc);
+        }
+
+        private void Test3_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtSzerokosc.Text, out double szerokosc) &&
+                double.TryParse(txtWysokosc.Text, out double wysokosc))
+            {
+                Prostokat(szerokosc, wysokosc, out double pole, out double obwod, out double przekatna);
+
+                string wynik = $"Pole: {pole:F2}\nObwód: {obwod:F2}\nPrzekątna: {przekatna:F2}";
+                MessageBox.Show(wynik, "Wyniki", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź poprawne liczby!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
