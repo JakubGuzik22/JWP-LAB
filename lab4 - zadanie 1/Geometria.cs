8f859f1881ec1f74a8380eddec1f9a8ff79327fa
JakubGuzik22 <148846333+JakubGuzik22@users.noreply.github.com>
2025-04-10 12:43:10 +0200

    Add files via upload

---
+++ lab4 - zadanie 1/Geometria.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace lab4___zadanie_1 //Geometria
+{
+    public interface IWyświetl
+    {
+        string PobierzIdentyfikator();
+    }
+    public static class ListBoxExtensions
+    {
+        public static void Dodaj(this ListBox listBox, IWyświetl obiekt)
+        {
+            listBox.Items.Add(obiekt.PobierzIdentyfikator());
+        }
+    }
+    public class Student : IWyświetl
+    {
+        public string Imię { get; set; }
+        public string Nazwisko { get; set; }
+
+        public Student(string imię, string nazwisko)
+        {
+            Imię = imię;
+            Nazwisko = nazwisko;
+        }
+
+        public string PobierzIdentyfikator()
+        {
+            return $"{Imię} {Nazwisko}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Student innyStudent)
+            {
+                return this.Nazwisko.CompareTo(innyStudent.Nazwisko);
+            }
+            return -1;
+        }
+    }
+    public abstract class Bryła: IWyświetl
+    {
+        public string Nazwa { get; }
+        public double Gęstość { get; }
+        public double CenaZaKg { get; }
+
+        public Bryła(string nazwa, double gęstość, double cenaZaKg)
+        {
+            Nazwa = nazwa;
+            Gęstość = gęstość;
+            CenaZaKg = cenaZaKg;
+        }
+
+        public abstract double ObliczObjętość();
+        public double ObliczMasę() {
+            return ObliczObjętość() * Gęstość;
+        }
+        public double ObliczCenę()
+        {
+            return ObliczMasę() * CenaZaKg;
+        }
+
+        public string PobierzIdentyfikator()
+        {
+            return $"Nazwa: {Nazwa:F2}, \nGęstość: {Gęstość:F2} kg/m³, \nCena za kg: {CenaZaKg:F2} PLN, " +
+                   $"\nObjętość: {ObliczObjętość():F2} m³, \nMasa: {ObliczMasę():F2} kg, \nCena: {ObliczCenę():F2} PLN";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Bryła innaBryla)
+            {
+                return innaBryla.ObliczObjętość().CompareTo(this.ObliczObjętość());
+            }
+            return 1;
+        }
+    }
+
+    public class Kula : Bryła
+    {
+        public double Promień { get; }
+
+        public Kula(string nazwa, double gęstość, double cenaZaKg, double promień)
+            : base(nazwa, gęstość, cenaZaKg)
+        {
+            Promień = promień;
+        }
+
+        public override double ObliczObjętość()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(Promień, 3);
+        }
+    }
+
+    public class Stożek : Bryła
+    {
+        public double Promień { get; }
+        public double Wysokość { get; }
+
+        public Stożek(string nazwa, double gęstość, double cenaZaKg, double promień, double wysokość)
+            : base(nazwa, gęstość, cenaZaKg)
+        {
+            Promień = promień;
+            Wysokość = wysokość;
+        }
+
+        public override double ObliczObjętość()
+        {
+            return (1.0 / 3.0) * Math.PI * Math.Pow(Promień, 2) * Wysokość;
+        }
+    }
+
+
+}
---
+++ lab4 - zadanie 1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4___zadanie_1
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOblicz_Click(object sender, RoutedEventArgs e)
+        {
+            double w, h, P, O;
+            DaneWejściowe okno1 = new DaneWejściowe();
+            okno1.ShowDialog();
+            w = Convert.ToDouble(okno1.Szerokość);
+            h = Convert.ToDouble(okno1.Wysokość);
+            P = w * h;
+            O = 2 * w + 2 * h;
+
+            DaneWyjściowe okno2 = new DaneWyjściowe(P, O);
+            okno2.ShowDialog();
+        }
+
+        private void btnKula_Click(object sender, RoutedEventArgs e)
+        {
+            Kula kula = new Kula("Kula", 50, 20, 5);
+            lblWyniki.Content = kula.ToString();
+        }
+
+        private void btnStożek_Click(object sender, RoutedEventArgs e)
+        {
+            Stożek stozek = new Stożek("Stożek", 10, 10, 3, 7);
+            lblWyniki.Content = stozek.ToString();
+        }
+
+        private void btnStudent_Click(object sender, RoutedEventArgs e)
+        {
+            List<IWyświetl> listaObiektów = new List<IWyświetl>();
+
+            listaObiektów.Add(new Kula("Kula", 50, 40, 5));
+            listaObiektów.Add(new Kula("Kula", 70, 20, 53));
+            listaObiektów.Add(new Stożek("Stożek", 10, 10, 3, 7));
+            listaObiektów.Add(new Student("Jan", "Kowalski"));
+            listaObiektów.Add(new Student("Anna", "Nowak"));
+            listaObiektów.Add(new Student("Piotr", "Wiśniewski"));
+
+            //listaObiektów.Sort();
+
+            foreach (var obiekt in listaObiektów)
+            {
+                listBox.Items.Add(obiekt.PobierzIdentyfikator());
+            }
+        }
+    }
+}
