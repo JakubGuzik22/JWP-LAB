acf9a5cb1fc47a7a85d5ea48c0a3be38af37e5f3
JakubGuzik22 <148846333+JakubGuzik22@users.noreply.github.com>
2025-04-10 11:58:53 +0200

    Add files via upload

---
+++ lab4 - zadanie 1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4___zadanie_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOblicz_Click(object sender, RoutedEventArgs e)
+        {
+            double w, h, P, O;
+            DaneWejściowe okno1 = new DaneWejściowe();
+            okno1.ShowDialog();
+            w = Convert.ToDouble(okno1.Szerokość);
+            h = Convert.ToDouble(okno1.Wysokość);
+            P = w * h;
+            O = 2*w + 2*h;
+
+            DaneWyjściowe okno2 = new DaneWyjściowe(P,O);
+            okno2.ShowDialog();
+        }
+
+        private void btnKula_Click(object sender, RoutedEventArgs e)
+        {
+            Kula kula = new Kula("Kula", 50, 20, 5);
+            lblWyniki.Content = kula.ToString();
+        }
+
+        private void btnStożek_Click(object sender, RoutedEventArgs e)
+        {
+            Stożek stozek = new Stożek("Stożek", 10, 10, 3, 7);
+            lblWyniki.Content = stozek.ToString();
+        }
+    }
+}
