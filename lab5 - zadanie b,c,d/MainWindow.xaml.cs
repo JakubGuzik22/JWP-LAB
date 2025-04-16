c135b71a1bb530b6e0c6367db67fecf6f9efa760
JakubGuzik22 <148846333+JakubGuzik22@users.noreply.github.com>
2025-04-16 14:38:50 +0200

    Add files via upload

---
+++ lab5 - zadanie b,c,d/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Generyki;
+
+namespace lab_5___zadanie_B
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<int, Student> studenci;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            studenci = new Dictionary<int, Student>
+            {
+                { 12345, new Student("Kowalski", 4.5) },
+                { 23456, new Student("Nowak", 5.0) },
+                { 34567, new Student("Wiśniewski", 3.8) },
+                { 45678, new Student("Lewandowski", 4.2) }
+            };
+        }
+
+        private void btnSzukaj_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtNrAlbumu.Text, out int numerAlbumu))
+            {
+                if (studenci.ContainsKey(numerAlbumu))
+                {
+                    Student student = studenci[numerAlbumu];
+                    MessageBox.Show($"Student: {student.Nazwisko}, Ocena: {student.Ocena}");
+                }
+                else
+                {
+                    MessageBox.Show("Nie znaleziono");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Proszę podać poprawny numer albumu.");
+            }
+        }
+
+        private void btnPorównaj_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 10;
+            int b = 20;
+
+            int result = Porównania.ZnajdźWiększy(a, b);
+            MessageBox.Show($"Większa liczba: {result}");
+
+            string c = "Jabłko";
+            string d = "Gruszka";
+
+            string result2 = Porównania.ZnajdźWiększy(c, d);
+            MessageBox.Show($"Większy ciąg: {result2}");
+
+
+            Student student1 = new Student("Kowalski", 6.5);
+            Student student2 = new Student("Nowak", 5.0);
+
+            Student resultStudent = Porównania.ZnajdźWiększy(student1, student2);
+            MessageBox.Show($"Większy student: {resultStudent.Nazwisko}, ocena: {resultStudent.Ocena}");
+        }
+
+        private void btnRegały_Click(object sender, RoutedEventArgs e)
+        {
+            Regał<double> regałDouble = new Regał<double>();
+            regałDouble.Półka1 = 3.14;
+            //regałDouble.WolnaPółka = 40.40;
+            regałDouble.WstawNaWolnąPółkę(2.71);
+            //regałDouble.WstawNaWolnąPółkę(30.30);
+            regałDouble.WolnaPółka = 1.41;
+            MessageBox.Show(regałDouble.ToString());
+
+            //sprawdzzemie czy wolna półka
+            regałDouble.WolnaPółka = 15.23;
+            MessageBox.Show(regałDouble.ToString());
+
+
+            Regał<int> regałInt = new Regał<int>();
+            regałInt.Półka1 = 3;
+            regałInt.WstawNaWolnąPółkę(2);
+            regałInt.WolnaPółka = 1;
+            MessageBox.Show(regałInt.ToString());
+
+            Regał<Student> regałStudent = new Regał<Student>();
+            regałStudent.Półka1 = new Student("Kowalski", 4.5);
+            regałStudent.WstawNaWolnąPółkę(new Student("Nowak", 5.0));
+            regałStudent.WolnaPółka = new Student("Wiśniewski", 3.8);
+            MessageBox.Show(regałStudent.ToString());
+        }
+    }
+}
---
+++ lab5 - zadanie b,c,d/Student.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Generyki
+{
+    internal class Student:IComparable<Student>
+    {
+        public string Nazwisko { get; set; }
+        public double Ocena { get; set; }
+
+        public Student(string nazwisko, double ocena)
+        {
+            Nazwisko = nazwisko;
+            Ocena = ocena;
+        }
+        public int CompareTo(Student other)
+        {
+            if (other == null) return 1;
+            return Ocena.CompareTo(other.Ocena);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nazwisko} ({Ocena})";
+        }
+    }
+
+
+    public static class Porównania
+    {
+        public static T ZnajdźWiększy<T>(T a, T b) where T : IComparable<T>
+        {
+            if (a.CompareTo(b) > 0)
+                return a;
+            else if (a.CompareTo(b) < 0)
+                return b;
+            else
+                return a;
+        }
+    }
+
+    public class Regał<T> where T : IComparable<T>
+    {
+        public T Półka1 { get; set; } = default!;
+        public T Półka2 { get; set; } = default!;
+        public T Półka3 { get; set; } = default!;
+
+        public Regał() { }
+
+        public override string ToString()
+        {
+            return $"Półka1: {Półka1}, \nPółka2: {Półka2}, \nPółka3: {Półka3}";
+        }
+
+        public void WstawNaWolnąPółkę(T item)
+        {
+            if (Półka1 == null || Półka1.Equals(default(T))) 
+            {
+                Półka1 = item;
+            }
+            else if (Półka2 == null ||Półka2.Equals(default(T)))
+            {
+                Półka2 = item;
+            }
+            else if (Półka3 == null || Półka3.Equals(default(T)))
+            {
+                Półka3 = item;
+            }
+            else
+            {
+                MessageBox.Show("Brak wolnej półki.");
+            }
+        }
+        public T WolnaPółka
+        {
+            set
+            {
+                WstawNaWolnąPółkę(value);
+            }
+        }
+    }
+}
+
