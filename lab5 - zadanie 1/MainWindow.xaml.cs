ea5741ef57f19bbe07958ad983a6c231bd8095b1
JakubGuzik22 <148846333+JakubGuzik22@users.noreply.github.com>
2025-04-16 13:23:36 +0200

    Add files via upload

---
+++ lab5 - zadanie 1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laboratorium5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Stozek_ZdarzenieBłędu(string opisBłędu)
+        {
+            MessageBox.Show(opisBłędu, "Błąd");
+
+            lblOpisBłędu.Content = $"Opis błędu: \n {opisBłędu}";
+        }
+        private void btnUtwórzStożek_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double promień = Convert.ToDouble(txtPromień.Text);
+                double wysokość = Convert.ToDouble(txtWysokość.Text);
+
+                Stożek stozek = new Stożek();
+
+                stozek.ZdarzenieBłędu += Stozek_ZdarzenieBłędu;
+
+                stozek.Promień = promień;
+                stozek.Wysokość = wysokość;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Proszę podać prawidłowe liczby w polach promienia i wysokości.", "Błąd danych");
+            }
+        }
+    }
+}
---
+++ lab5 - zadanie 1/Stożek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorium5
+{
+    internal class Stożek
+    {
+        public delegate void ObsługaBłędu(string opisBłędu);
+
+        public event ObsługaBłędu ZdarzenieBłędu;
+
+        private double promień;
+        private double wysokość;
+
+        public double Promień
+        {
+            get => promień;
+            set
+            {
+                if (value < 0)
+                {
+                    if (ZdarzenieBłędu != null)
+                    {
+                        ZdarzenieBłędu("Promień nie może być ujemny.");
+                    }
+                }
+                else
+                {
+                    promień = value;
+                }
+            }
+        }
+
+        public double Wysokość
+        {
+            get => wysokość;
+            set
+            {
+                if (value < 0)
+                {
+                    if (ZdarzenieBłędu != null)
+                    {
+                        ZdarzenieBłędu("Wysokość nie może być ujemna.");
+                    }
+                }
+                else
+                {
+                    wysokość = value;
+                }
+            }
+        }
+    }
+}
